bc1ed5db45d87e425d8742ffd7104d39651d967b
Tyoma Zagoskin <a.zagoskin@napoleonit.ru>
2020-01-16 22:11:09 +0500

    Refactor Profile View Controller

--- Violations/Scenes/Profile info/ProfileViewController.swift
+++ Violations/Scenes/Profile info/ProfileViewController.swift
@@ -10,38 +10,37 @@ import UIKit
 import Firebase
 
 
-class ProfileViewController: UIViewController, ProgressHUDShowing {
-
-    private var numberOfMarkers = 0
-    private var user: User? { Auth.auth().currentUser }
+final class ProfileViewController: UIViewController, ProgressHUDShowing {
     
-    @IBOutlet weak var logoutButton: PrimaryButton! {
+    // MARK: Outlets
+    
+    @IBOutlet private weak var logoutButton: PrimaryButton! {
         didSet {
             logoutButton.layer.borderColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
         }
     }
     
-    @IBOutlet weak var infoLabel: UILabel! {
+    @IBOutlet private weak var infoLabel: UILabel! {
         didSet {
             infoLabel.text = ""
             infoLabel.textColor = Theme.current.textColor
         }
     }
     
-    @IBOutlet weak var themeLabel: UILabel! {
+    @IBOutlet private weak var themeLabel: UILabel! {
         didSet {
             themeLabel.textColor = Theme.current.textColor
         }
     }
-    @IBOutlet weak var themeSwitcher: UISwitch! {
+    @IBOutlet private weak var themeSwitcher: UISwitch! {
         didSet {
             themeSwitcher.isOn = !Theme.isLightTheme
         }
     }
     
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
+    // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,15 +49,21 @@ class ProfileViewController: UIViewController, ProgressHUDShowing {
         applyTheme()
     }
     
-    @IBAction func logoutPressed(_ sender: Any) {
-        logoutButton.startAnimation()
+}
+
+
+// MARK: - Actions
+
+extension ProfileViewController {
+    
+    @IBAction private func logoutPressed(_ sender: PrimaryButton) {
+        sender.startAnimation()
         do {
             try Auth.auth().signOut()
-        UIApplication.setRootView(NavigationViewController.instantiate(from: .Login), options: UIApplication.logoutAnimation)
-
+            UIApplication.setRootView(NavigationViewController.instantiate(from: .Login), options: UIApplication.logoutAnimation)
         }
         catch {
-            logoutButton.stopAnimation(animationStyle: .shake) {
+            sender.stopAnimation(animationStyle: .shake) {
                 self.showProgressHUDError(with: "Something went wrong")
                 self.dismissProgressHUD()
             }
@@ -66,45 +71,50 @@ class ProfileViewController: UIViewController, ProgressHUDShowing {
 
     }
     
-    @IBAction func changeTheme(_ sender: UISwitch) {
+    @IBAction private func changeTheme(_ sender: UISwitch) {
         Theme.isLightTheme = !sender.isOn
         
         applyTheme()
     }
     
-    func retrieveMarkers() {
-        
-        TakeMarkersFromFirebase.downloadMarkers(child: "Markers") {snapshotValue in
-            let sender = snapshotValue["Sender"]!
-            if sender == self.user?.email {
-                self.numberOfMarkers += 1
-            }
-            self.activityIndicator.stopAnimating()
-            self.infoLabel.text = self.numberOfMarkersInfo()
-        }
-    }
-    
-    func numberOfMarkersInfo() -> String {
-        switch self.numberOfMarkers {
-        case 0:
-            return "You haven't reported any violations yet"
-        case 1:
-            return "You marked 1 violation!\nThanks for help!"
-        default:
-            return "You marked \(self.numberOfMarkers) violations!\nThanks for help!"
-        }
-    }
 }
 
 
-//MARK: - Change theme
+//MARK: - Private API
 
 extension ProfileViewController {
-    func applyTheme() {
+    
+    private func applyTheme() {
         view.backgroundColor = Theme.current.background
         themeLabel.textColor = Theme.current.textColor
         infoLabel.textColor = Theme.current.textColor
         self.tabBarController?.tabBar.barTintColor = Theme.current.tabBarColor
         self.tabBarController?.tabBar.unselectedItemTintColor = Theme.current.tabBarTintColor
     }
+    
+    private func retrieveMarkers() {
+        let user = Auth.auth().currentUser
+        var numberOfMarkers = 0
+        
+        TakeMarkersFromFirebase.downloadMarkers(child: "Markers") {snapshotValue in
+            let sender = snapshotValue["Sender"]!
+            if sender == user?.email {
+                numberOfMarkers += 1
+            }
+            self.activityIndicator.stopAnimating()
+            self.infoLabel.text = self.getInfoLabelText(with: numberOfMarkers)
+        }
+    }
+    
+    private func getInfoLabelText(with numberOfMarkers: Int) -> String {
+        switch numberOfMarkers {
+        case 0:
+            return "You haven't reported any violations yet"
+        case 1:
+            return "You marked 1 violation!\nThanks for help!"
+        default:
+            return "You marked \(numberOfMarkers) violations!\nThanks for help!"
+        }
+    }
+    
 }
